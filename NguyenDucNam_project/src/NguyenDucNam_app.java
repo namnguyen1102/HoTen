7e22b1d0101f346c1d1419bb8564e1db7a32b20b
namnguyen1102 <2121051294@student.humg.edu.vn>
2024-11-02 06:14:10 +0700

    Update NguyenDucNam_app.java

--- NguyenDucNam_project/src/NguyenDucNam_app.java
+++ NguyenDucNam_project/src/NguyenDucNam_app.java
@@ -1,68 +1,57 @@
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
-
-import java.io.IOException;
-
-public class ElectricityMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
-    private Text nam = new Text();
-    private LongWritable tieuThu = new LongWritable();
-
-    @Override
-    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String[] truong = value.toString().split(",");
-        nam.set(truong[0]);
-        long tong = 0;
-        int dem = 0;
-        for (int i = 1; i < truong.length - 1; i++) {
-            tong += Long.parseLong(truong[i]);
-            dem++;
-        }
-        long trungBinh = tong / dem;
-        tieuThu.set(trungBinh);
-        context.write(nam, tieuThu);
-    }
-}
-
-
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Reducer;
-
-import java.io.IOException;
-
-public class ElectricityReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
-    private LongWritable ketQua = new LongWritable();
-
-    @Override
-    protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
-        for (LongWritable val : values) {
-            if (val.get() > 30) {
-                ketQua.set(val.get());
-                context.write(key, ketQua);
-            }
-        }
-    }
-}
-
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class ElectricityDriver {
+import java.io.IOException;
+
+public class DienTieuThu {
+
+    public static class ConsumptionMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+        private final static IntWritable consumptionValue = new IntWritable();
+        private Text year = new Text();
+
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String[] parts = value.toString().split("\\s+");
+            if (parts.length > 1) {
+                year.set(parts[0]); 
+                int lastValue = Integer.parseInt(parts[parts.length - 1]); 
+                consumptionValue.set(lastValue);
+                context.write(year, consumptionValue);
+            }
+        }
+    }
+
+    public static class ConsumptionReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private final static IntWritable result = new IntWritable();
+        private final static int threshold = 30;
+
+        @Override
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            for (IntWritable val : values) {
+                if (val.get() > threshold) {
+                    result.set(val.get());
+                    context.write(key, result);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "tieu thu dien");
-        job.setJarByClass(ElectricityDriver.class);
-        job.setMapperClass(ElectricityMapper.class);
-        job.setReducerClass(ElectricityReducer.class);
+        Job job = Job.getInstance(conf, "electricity consumption analysis");
+        job.setJarByClass(DienTieuThu.class);
+        job.setMapperClass(ConsumptionMapper.class);
+        job.setReducerClass(ConsumptionReducer.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(LongWritable.class);
+        job.setOutputValueClass(IntWritable.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
