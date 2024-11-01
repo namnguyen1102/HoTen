51fc8bde10a33d063f393ae21f97047f15d67558
namnguyen1102 <2121051294@student.humg.edu.vn>
2024-11-02 05:45:32 +0700

    Update NguyenDucNam_app.java

--- NguyenDucNam_project/src/NguyenDucNam_app.java
+++ NguyenDucNam_project/src/NguyenDucNam_app.java
@@ -1,69 +1,70 @@
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
 import java.io.IOException;
 
-public class ElectricMapper extends MapReduceBase implements Mapper<LongWritable, /*Input key Type*/
-Text, /*Input value Type*/
-Text, /*Output key Type*/
-IntWritable /*Output value Type*/ 
-{
-//Map function
-public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
-    String line = value.toString();
-    String lasttoken = null;
-    StringTokenizer s = new StringTokenizer(line, "\t");
-    String year = s.nextToken();
+public class ElectricityMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+    private Text nam = new Text();
+    private LongWritable tieuThu = new LongWritable();
 
-    while(s.hasMoreTokens()) {
-        lasttoken = s.nextToken();
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] truong = value.toString().split(",");
+        nam.set(truong[0]);
+        long tong = 0;
+        int dem = 0;
+        for (int i = 1; i < truong.length - 1; i++) {
+            tong += Long.parseLong(truong[i]);
+            dem++;
+        }
+        long trungBinh = tong / dem;
+        tieuThu.set(trungBinh);
+        context.write(nam, tieuThu);
     }
-
-    int avgprice = Integer.parseInt(lasttoken);
-    output.collect(new Text(year), new IntWritable(avgprice));
-}
 }
 
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 
-public abstract class ElectricReducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+public class ElectricityReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
+    private LongWritable ketQua = new LongWritable();
 
-    // Reduce function
-    public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
-        int maxavg = 30;
-        int val = Integer.MIN_VALUE;
-        while (values.hasNext()) {
-            if ((val = values.next().get()) >= maxavg) {
-                output.collect(key, new IntWritable(val));
+    @Override
+    protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+        for (LongWritable val : values) {
+            if (val.get() > 30) {
+                ketQua.set(val.get());
+                context.write(key, ketQua);
             }
         }
     }
 }
 
-import java.util.*;
-import java.io.IOException;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapred.*;
-import org.apache.hadoop.util.*;
 
-public class ElectricCalculator {
-    public static void main(String args[]) throws Exception {
-        JobConf conf = new JobConf(ElectricCalculator.class);
-        
-        conf.setJobName("unis_above_30");
-        
-        conf.setOutputKeyClass(Text.class);
-        conf.setOutputValueClass(IntWritable.class);
-        
-        conf.setMapperClass(ElectricMapper.class);
-        conf.setCombinerClass(ElectricReducer.class);
-        conf.setReducerClass(ElectricReducer.class);
-        
-        conf.setInputFormat(TextInputFormat.class);
-        conf.setOutputFormat(TextOutputFormat.class);
-        
-        FileInputFormat.setInputPaths(conf, new Path(args[0]));
-        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
-        
-        JobClient.runJob(conf);
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class ElectricityDriver {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "tieu thu dien");
+        job.setJarByClass(ElectricityDriver.class);
+        job.setMapperClass(ElectricityMapper.class);
+        job.setReducerClass(ElectricityReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
